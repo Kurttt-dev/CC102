377176044299345a725d6b39414dcc7d7e2baad4
Kurttt-dev <kurtmaranon06@gmail.com>
2024-10-03 10:09:35 +0800

    Add files via upload

---
+++ 02BasicOutput (1).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Our lady of lourdes college foundation" << endl;
+   cout << "College of computer studies" << endl;
+   cout << "Bachelor of science in information technology 1-B" << endl;
+   cout << "Kurt bennydick Marañon" << endl;
+   cout << "Purok-3 Calero, Jose Panganiban Camarines Norte" << endl;
+   return 0;
+}   
