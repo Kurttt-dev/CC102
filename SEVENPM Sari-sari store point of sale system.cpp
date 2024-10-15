fc32b9136865a4c3af57b01130a8f270e5c5e09f
Kurttt-dev <kurtmaranon06@gmail.com>
2024-10-15 10:52:31 +0800

    Update SEVENPM Sari-sari store point of sale system.cpp

--- SEVENPM Sari-sari store point of sale system.cpp
+++ SEVENPM Sari-sari store point of sale system.cpp
@@ -3,12 +3,24 @@ using namespace std;
 
 int main()
 {       
-     float sugarPriceUSD=3.00, ricePricePound=1.06, sardinesPricePound=0.57, coffeePriceUSD=3.23, milkPriceUSD=4.12;
-	 float USDtoPHP, PoundstoPHP, TotalPHP;
-	 float sugarinPHP=567, riceinPHP=60.91, sardinesinPHP=40, coffeeinPHP=160, milkinPHP=206;
+         float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
+	 float USDtoPHP, PoundtoPHP, TotalPHP;
+	 float SugarinPHP, RiceinPHP, SardinesinPHP, CoffeeinPHP, MilkinPHP;
 	 int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
-	 
-	 cout << "Enter the quantity of sugar:";
+	 	
+	 cout << "Price of sugar in PHP:";	 
+	 cin >> sugarPriceUSD;
+	 cout << "Price of rice in PHP:";	 
+	 cin >> ricePricePound;
+	cout << "Price of sardines in PHP:";	 
+	 cin >> sardinesPricePound;
+	cout << "Price of coffee in PHP:";
+	cin >> coffeePriceUSD;
+	 cout << "Price of milk in PHP:";
+	cin >> milkPriceUSD;
+	
+	
+	cout << "Enter the quantity of sugar:";
 	 cin >> sugarQty;
 	 cout << "Enter the quantity of rice:";
 	 cin >> riceQty;
@@ -17,17 +29,16 @@ int main()
 	 cout << "Enter the quantity of coffee:";
 	 cin >> coffeeQty;
 	 cout << "Enter the quantity of milk:";
-	 cin >> milkQty;
-	  
-	 //usd conversion//
-	 USDtoPHP, PoundstoPHP, TotalPHP;
-	 
-	 cout << "Price of sugar in PHP:";	 
-	 cout << "Price of rice in PHP:";	 
-	 cout << "Price of sardines in PHP:";	 
-	 cout << "Price of coffee in PHP:";	 
-	 cout << "Price of milk in PHP:";	 
-	 cout << "TotalPHP:";
-	 cin >> TotalPHP;
-	 return 0;
+	 cin >> milkQty;	  	 
+
+	SugarinPHP = sugarPriceUSD * USDtoPHP * sugarQty;
+	RiceinPHP = ricePricePound * PoundtoPHP * riceQty;
+	SardinesinPHP = sardinesPricePound * PoundtoPHP * sardinesQty;
+	CoffeeinPHP = coffeePriceUSD * USDtoPHP * coffeeQty;
+	MilkinPHP = milkPriceUSD * USDtoPHP * milkQty;
+
+	TotalPHP = SugarinPHP + RiceinPHP + SardinesinPHP + CoffeeinPHP + MilkinPHP;
+
+	cout << "Total price in PHP: " << TotalPHP << endl;	
+	return 0;
 }	 
